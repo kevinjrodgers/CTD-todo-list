75d6d3a81d956145878d642f4a3794fff5ef318b
unknown <kevjrodgers@gmail.com>
2026-08-25 20:43:23 -0700

    Re-added console logs

--- src/features/Todos/TodosPage.jsx
+++ src/features/Todos/TodosPage.jsx
@@ -184,6 +184,7 @@ function TodosPage({ token }) {
   }
 
   const invalidateCache = useCallback(() => {
+    console.log('Invaliding memo cache after todo mutation');
     setDataVersion(prev => prev + 1);
   }, []);
 
