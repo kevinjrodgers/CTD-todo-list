aea97fa7f07582435da75214e33f0273863e3c02
unknown <kevjrodgers@gmail.com>
2026-08-28 19:03:36 -0700

    Initial setup for Part 2. Created todoReducer, partially started FETCH_START action

---
+++ src/reducers/todoReducer.js
@@ -0,0 +1,48 @@
+export const TODO_ACTIONS = {
+  // Fetch operations
+  FETCH_START: 'FETCH_START',
+  FETCH_SUCCESS: 'FETCH_SUCCESS',
+  FETCH_ERROR: 'FETCH_ERROR',
+  
+  // Add todo operations
+  ADD_TODO_START: 'ADD_TODO_START',
+  ADD_TODO_SUCCESS: 'ADD_TODO_SUCCESS',
+  ADD_TODO_ERROR: 'ADD_TODO_ERROR',
+
+  // Complete todo operations
+  COMPLETE_TODO_START: 'COMPLETE_TODO_START',
+  COMPLETE_TODO_SUCCESS: 'COMPLETE_TODO_SUCCESS',
+  COMPLETE_TODO_ERROR: 'COMPLETE_TODO_ERROR',
+
+  // Update todo operation
+  UPDATE_TODO_START: 'UPDATE_TODO_START',
+  UPDATE_TODO_SUCCESS: 'UPDATE_TODO_SUCCESS',
+  UPDATE_TODO_ERROR: 'UPDATE_TODO_ERROR',
+
+};
+
+export const initialState = {
+  todoList: [],
+  error: '',
+  filterError: '',
+  isTodoListLoading: false, // Instructions show this as an initial value of true?
+  sortBy: 'createdAt',
+  sortDirection: 'desc', // Instructions show this as an initial value of asc
+  filterTerm: '',
+  dataVersion: 0,
+};
+
+export function todoReducer(state, action) {
+  switch(action.type) {
+    // Add more cases here
+    case TODO_ACTIONS.FETCH_START:
+      return {
+        ...TODO_ACTIONS,
+        isTodoListLoading: true,
+        error: '',
+        filterError: '',
+      }
+    default: 
+      throw new Error(`Unknown action type: ${action.type}`);
+  }
+}
